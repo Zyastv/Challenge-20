cf8a3d22d1843504458672d4a38dbf96da9cd758
Zachary Adams <Zach2vegas@gmail.com>
2022-12-02 19:18:47 -0800

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Project from './components/Project'
+
+
+function App() {
+
+  return (
+    <div className="portfolio-app">
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
